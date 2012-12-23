a76607f4697aacbaf6ec02094d4e53224c7e6fb8
dengjing34 <dengjing34@vip.qq.com>
2012-12-23 12:43:39 +0800

    add auth for front pages

---
+++ view/base/login.php
@@ -0,0 +1,14 @@
+<?php
+//dengjing34@vip.qq.com
+?>
+    <div id="nav">
+        <div id="nav-inner">
+            <form method="post">
+            <ul>
+                <li><label for="userName">用户名：<label><input type="text" name="userName" id="userName" /></li>
+                <li><label for="password">密码：</label><input type="password" name="password" id="password" /></li>
+            </ul>
+            </form>    
+            <div class="clear"></div>
+        </div>
+    </div>
